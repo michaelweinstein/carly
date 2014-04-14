a0a8bf309677425839f427e6e956b15c7389039d
Evan Fuller <evan_fuller@brown.edu>
2014-04-14 14:25:21 -0400

    Added stub for TimeAllocator class, added backend package

---
+++ src/backend/TimeAllocator.java
@@ -0,0 +1,12 @@
+package backend;
+
+
+/**
+ * A class that will examine a user's current calendar of events and plot the optimal schedule,
+ * given some recent change or new assignment to add to the calendar.
+ * @author evanfuller
+ *
+ */
+public class TimeAllocator {
+
+}
