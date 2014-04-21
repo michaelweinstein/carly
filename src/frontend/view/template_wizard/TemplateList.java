4ab23ebfd1cbb5a7a903cca99185ecf2cbbc3e13
Michael Weinstein <michael_weinstein@brown.edu>
2014-04-21 01:04:17 -0400

    Added some print lines in TemplateList, but commented them out for now

--- src/frontend/view/template_wizard/TemplateList.java
+++ src/frontend/view/template_wizard/TemplateList.java
@@ -10,19 +10,16 @@ import data.Template;
 // TODO
 public class TemplateList {
 	
-	private static List<ITemplate> _templates;
+	private static List<ITemplate> _templates = new ArrayList<>();
 	
 	public TemplateList() {
 		_templates = StorageService.getAllTemplates();
-///// Until StorageService method is written
-		if (_templates == null) 
-			_templates = new ArrayList<ITemplate>();
 		
-/////		
-		System.out.println("_templates.size(): " + _templates.size());
+//////////////	
+/*		System.out.println("_templates.size(): " + _templates.size());
 		for (ITemplate t: _templates) {
 			System.out.println("t: " + t);
-		}
+		}*/
 	}
 	
 	/**
@@ -31,7 +28,7 @@ public class TemplateList {
 	 */
 	public static void addTemplate(Template t) {
 //////
-		System.out.println("template: " + t);
+//		System.out.println("template: " + t);
 		
 		StorageService.addTemplate(t);
 		_templates.add(t);
