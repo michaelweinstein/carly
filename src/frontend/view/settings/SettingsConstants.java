84bceb195411ae6ba26cb0d18afaf85dc38a6bde
michaelweinstein <michael_weinstein@brown.edu>
2014-05-03 18:27:48 -0400

    Added StorageService calls to StartupView.submitSurvey for preferences, and created infrastructor to access data for user-selected timeblocks for getAllUnavailableBlocks in SurveyWeekView

---
+++ src/frontend/view/settings/SettingsConstants.java
@@ -0,0 +1,23 @@
+package frontend.view.settings;
+
+/**
+ * Class that stores <code>String</code> constants used
+ * as keys to store Settings values in SQL database.
+ * 
+ * @author michaelweinstein
+ *
+ */
+
+public class SettingsConstants {
+	
+	/**
+	 * String key storing user's preferred Time of Day
+	 */
+	public static final String TIMEOFDAY_SETTING = "tod";
+	
+	/**
+	 * String key storing whether Learner is enabled
+	 */
+	public static final String LEARNER_SETTING = "learner";
+
+}
--- src/frontend/view/startup/StartupView.java
+++ src/frontend/view/startup/StartupView.java
@@ -4,14 +4,19 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 
+import backend.database.StorageService;
 import data.TimeOfDay;
+import data.UnavailableBlock;
 import frontend.Utils;
+import frontend.view.settings.SettingsConstants;
 import frontend.view.startup.timepicker.SurveyWeekView;
 
 public class StartupView extends JDialog {
@@ -19,7 +24,7 @@ public class StartupView extends JDialog {
 	private static final long serialVersionUID = -3311099840458252581L;
 	
 	/* Styling vars */
-	private static final Dimension minimum_size = new Dimension(600, 750);
+	private static final Dimension minimum_size = new Dimension(600, 800);
 	// @params: top, left, bottom, right
 	private static final Insets insets = new Insets(10, 5, 10, 5);	
 	private static final int padding = 15;
@@ -30,7 +35,14 @@ public class StartupView extends JDialog {
 	private static final String learner_label = 
 				"Would you like settings to be adjusted based on your behavior?";
 	private static final String time_label =
-				"Please drag for the times you are available during the average week:";
+				"Please drag for the times you are available during the average week." + 
+						"This template is final!";
+	private static final String submit_label = "Submit survey";
+	
+	/* Input fields */
+	private JComboBox<TimeOfDay> _todPicker;
+	private JCheckBox _learnerCheck;
+	private SurveyWeekView _timeView;
 	
 	public StartupView() {
 		super();
@@ -49,48 +61,76 @@ public class StartupView extends JDialog {
 		c.weightx = 1;
 		c.weighty = 0;
 		c.fill = GridBagConstraints.VERTICAL;
-		int currRow = 0;
 		
 		/* "When do you prefer to work during the day?" */
 		JLabel todLabel = new JLabel(hours_label);
-		JComboBox<TimeOfDay> todPicker = new JComboBox<>(TimeOfDay.values());
-		todPicker.setEditable(false);	// TOD values never modified
+		_todPicker = new JComboBox<>(TimeOfDay.values());
+		_todPicker.setEditable(false);	// TOD values never modified
 		Utils.themeComponent(todLabel);
 		c.gridx = 0;
-		c.gridy = currRow+=1;
+		c.gridy = 0;
 		c.anchor = GridBagConstraints.EAST;
 		this.add(todLabel, c);
 		c.gridx = 1;
 		c.anchor = GridBagConstraints.WEST;
-		this.add(todPicker, c);
+		this.add(_todPicker, c);
 		
 		/* "Would you like settings to be adjusted based on your behavior?" */
 		JLabel learnerLabel = new JLabel(learner_label);
-		JCheckBox learnerCheck = new JCheckBox();
+		_learnerCheck = new JCheckBox();
 		Utils.themeComponent(learnerLabel);
 		c.gridx = 0;
-		c.gridy = currRow+=1;
+		c.gridy = 1;
 		c.anchor = GridBagConstraints.EAST;
 		this.add(learnerLabel, c);
 		c.gridx = 1;
 		c.anchor = GridBagConstraints.WEST;
-		this.add(learnerCheck, c);
+		this.add(_learnerCheck, c);
 		
-		// TODO Week graphic and setting available time template
+		/* "Set available times during average week" */
 		JLabel timeLabel = new JLabel(time_label);
-		SurveyWeekView timeView = new SurveyWeekView();
+		_timeView = new SurveyWeekView();
 		Utils.themeComponent(timeLabel);
 		c.gridx = 0;
-		c.gridy = currRow+=1;
+		c.gridy = 2;
 		c.gridwidth = 2;
 		c.anchor = GridBagConstraints.CENTER;
 		this.add(timeLabel, c);
 		c.gridx = 0;
-		c.gridy = currRow+=1;
-		this.add(timeView, c);
+		c.gridy = 3;
+		this.add(_timeView, c);
 		
-		
+		JButton submitBtn = new JButton(submit_label);
+		submitBtn.setPreferredSize(new Dimension(20, 200));
+		c.gridx = 0;
+		c.gridy = 4;
+		c.gridwidth = 1;
+//		c.anchor = GridBagConstraints.WEST;
+//		this.add(submitBtn, c);
 		
 		// TODO User cannot close unless survey is completed
+	}
+	
+	// TODO: Complete and comment
+	private void submitSurvey() {
+		// Add TOD preference
+		TimeOfDay todPref = (TimeOfDay) (_todPicker.getSelectedItem());
+		StorageService.mergeSetting(SettingsConstants.TIMEOFDAY_SETTING, todPref.name());
+		
+		// Add Learner preference
+		Boolean learnerPref = _learnerCheck.isSelected();
+		StorageService.mergeSetting(SettingsConstants.LEARNER_SETTING, learnerPref.toString());
+		
+		// Add UnavailableBlocks to StorageService
+		List<UnavailableBlock> uBlocks = _timeView.getUnavailableBlocks();
+		for (UnavailableBlock b: uBlocks) {
+/*			try {
+				StorageService.addTimeBlock(b);
+			} catch (StorageServiceException e) {
+				System.out.println("ERROR: StorageServiceException when adding " + 
+						"time blocks (StartupView.submitSurvey)");
+				e.printStackTrace();
+			}*/
+		}
 	}
 }
